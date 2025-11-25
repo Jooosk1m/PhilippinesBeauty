33cda566f04b4af2c7157d6a0905e5da568494cb
Jooosk1m <127473282+Jooosk1m@users.noreply.github.com>
2025-11-26 01:14:20 +0800

    Delete deploy-6397f1a48095c51d4674e8a9 directory

--- deploy-6397f1a48095c51d4674e8a9/wait.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function(){
-    var img = this.localStorage.getItem('img')
-    var p = this.localStorage.getItem('p')
-    var title = this.localStorage.getItem('title')
-
-    this.document.getElementById('ned1').src = img
-    this.document.getElementById('ned2').innerHTML = p
-    this.document.getElementById('ned3').innerHTML = title
-})
